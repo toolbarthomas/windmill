9833f98caff2e49fb95a9a505d4cc069e62407ec
Thomas van der velde <contact@toolbarthomas.net>
2018-11-02 16:58:38 +0100

    Send email trough ethereal.email

--- bin/mailer.js
+++ bin/mailer.js
@@ -1,3 +1,5 @@
+const Promise = require("bluebird");
+
 const cheerio = require("cheerio");
 const htmlToText = require("nodemailer-html-to-text").htmlToText;
 const nodemailer = require("nodemailer");
@@ -5,7 +7,7 @@ const path = require("path");
 
 const error = require("./error");
 const info = require("./info");
-const warning = require("./warning");
+const success = require("./success");
 
 module.exports = {
   /**
@@ -13,7 +15,7 @@ module.exports = {
    *
    * @param {Object} build Processed output from the subject template.
    */
-  send(html, subject, config) {
+  async send(html, subject, config) {
     if (!html) {
       return;
     }
@@ -23,40 +25,67 @@ module.exports = {
       error(`Unable to send the email since no recipient has been defined.`);
     }
 
+    // Get the extension name to filter out.
+    const extension = path.extname(subject);
+
+    // Define the filename for the processed template.
+    const subjectName = path.basename(subject, extension);
+
     // Define the subject line from the title element within from processed html.
     const $ = cheerio.load(html);
 
     // Define the subject
     // @todo fix title tag within the builder.
-    const subjectLine = $('title').text().trim() || `Example email from subject: ${path.basename(subject)}`;
+    const subjectLine = $('title').text().trim() || `Example email from subject: ${subjectName}`;
 
-    nodemailer.createTestAccount((err, account) => {
-      if (err) {
-        error(err);
-      }
+    // Define the SMTP credentials from the cache.
+    let credentials = {};
 
-      let transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
-        secure: false,
-        auth: {
-          user: account.user,
-          pass: account.pass
+    credentials = await new Promise((cb) => {
+      nodemailer.createTestAccount((err, account) => {
+        if (err) {
+          error(err);
         }
+
+        cb(account);
       });
+    });
 
-      // Extra the text of the generated html and use it as plain text email.
-      transporter.use('compile', htmlToText());
+    // Verify the generated credentials.
+    if (!credentials) {
+      error('No valid transporter credentials are defined, aborting Windmill.');
+    }
 
-      // Send the generated subject from ethereal.email.
-      transporter.sendMail({
-        from: config.sender,
-        subject: subjectLine,
-        to: config.recipients,
-        html: html
-      });
+    // Setup a text account for ethereal.email.
+    let transporter = nodemailer.createTransport({
+      host: credentials.smtp.host,
+      port: credentials.smtp.port,
+      secure: credentials.smtp.secure,
+      auth: {
+        user: credentials.user,
+        pass: credentials.pass
+      }
+    });
 
-      info(`Sending mail for: ${path.basename(subject)}`);
+    // Nodemailer required options.
+    const mailerOptions = {
+      from: config.sender,
+      subject: subjectLine,
+      to: config.recipients[0],
+      html: html
+    };
+
+    // Extract the text of the generated html and use it as plain text email.
+    transporter.use('compile', htmlToText());
+
+    info(`Sending mail for: ${subjectName}`);
+
+    // Send the generated subject from ethereal.email.
+    transporter.sendMail(mailerOptions).then((mailInfo) => {
+      info(`Message sent: ${mailInfo.messageId}`);
+      info(`See preview at: ${nodemailer.getTestMessageUrl(mailInfo)}`);
+
+      success(`Succecfully sent email for subject: ${subjectName}`);
     });
   }
 }
